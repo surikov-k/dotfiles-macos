5029d57d9fc8d5241043d65af1c73431604bf2a9
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-02-21 22:11:47 +0300

    Switch to Brewfile for Homebrew installs
    Install exa and Dropbox

--- setup-homebrew.zsh
+++ setup-homebrew.zsh
@@ -4,9 +4,4 @@ echo "\n <<< Starting Homebrew Setup 🍺 >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install httpie
-brew install bat
-
-brew install google-chrome
-brew install visual-studio-code
-brew install --no-quarantine alfred
+brew bundle --verbose
